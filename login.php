e51f6ddb6b37e1de37fcdeab41c0cb56b41cf8a3
huangyl <huangyl@woobest.com>
2025-07-03 13:21:41 +0800

    feat : 随便改

--- login.php
+++ login.php
@@ -21,7 +21,7 @@ if (isset($_POST['login'])) {
             $user_id = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$username))[0]['id'];
             DB::query('INSERT INTO login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$user_id));
 
-            setcookie("SNID", $token, time() + 60 * 60 * 24 * 7, '/', NULL, NULL, TRUE);
+            setcookie9("SNID", $token, time() + 60 * 60 * 24 * 7, '/', NULL, NULL, TRUE);
 
 
         } else {
@@ -36,9 +36,3 @@ if (isset($_POST['login'])) {
 
 
 ?>
-<h1>Login to your account</h1>
-<form action="login.php" method="post">
-<input type="text" name="username" value="" placeholder="Username ..."><p />
-<input type="password" name="password" value="" placeholder="Password ..."><p />
-<input type="submit" name="login" value="Login">
-</form>
--- logout.php
+++ logout.php
@@ -29,9 +29,3 @@ if (isset($_POST['confirm'])) {
 }
 
 ?>
-<h1>Logout of your Account?</h1>
-<p>Are you sure you'd like to logout?</p>
-<form action="logout.php" method="post">
-    <input type="checkbox" name="alldevices" value="alldevices"> Logout of all devices?<br />
-    <input type="submit" name="confirm" value="Confirm">
-</form>
